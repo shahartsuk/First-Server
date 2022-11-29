7772394470728ff8a5e3c88cc16bad89260d3b95
shahar tsuk <shahartzuk23@gmail.com>
2022-11-29 14:53:17 +0200

    fix

--- app.js
+++ app.js
@@ -14,24 +14,28 @@ const App = express();
 App.use(express.json());
 
 // "/" endpoint => http://localhost:3000
-App.get("/", function (req, res) {
+App.get("/", (req, res) => {
   res.send("Hello World");
 });
 
 // "/api/courses" endpoint => http://localhost:3000/api/courses
-App.get("/api/courses", function (req, res) {
+App.get("/api/courses", (req, res) => {
   res.send(courses);
 });
 
 // "/api/courses/number of id" endpoint => http://localhost:3000/api/courses
-App.get("/api/courses/:id", function (req, res) {
+App.get("/api/courses/:id", (req, res) => {
   let id = parseInt(req.params.id);
   let course = courses.find((c) => c.id === id);
   res.send(course);
 });
 
 // "/api/addCourses" endpoint => http://localhost:3000/api/courses
-App.post("/api/addCourses", function (req, res) {
+App.post("/api/addCourses", (req, res) => {
+  if (!req.body.title || req.body.title.length < 2) {
+    res.status(400).send({ msg: "title is invalid" });
+    return;
+  }
   let courseTitle = req.body.title;
   let newCourse = {
     id: courses.length + 1,
@@ -39,6 +43,13 @@ App.post("/api/addCourses", function (req, res) {
   };
   console.log(courseTitle);
   courses.push(newCourse);
+  res.send(courses);
+});
+
+App.delete("/api/courses/:id", (req, res) => {
+  let course = courses.find((c) => c.id === parseInt(req.params.id));
+  let indexOf = courses.indexOf(course);
+  courses.splice(indexOf, 1);
   res.send(courses);
 });
 
